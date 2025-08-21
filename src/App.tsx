5d05a24e0de665deb82f4eb010881817876e5c1f
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2025-08-21 10:14:05 +0200

    add latest package

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import HomePage from './pages/Home';
-import { CMSConfiguration, CMSProvider } from "myheadless-react";
+import { CMSConfig, CMSProvider } from "myheadless-react";
 
 function App() {
-  const config = new CMSConfiguration(
-    "https://myheadless.io/cms",
-    "your-api-key",
-  );
+  const config = new CMSConfig({
+    baseURL: "https://myheadless.io/cms",
+    apiKey: "your-api-key",
+  });
   return (
     <CMSProvider config={config}>
       <HomePage />
